0bdff467cd4e8eeb05ce065ac38ba4d48928c1da
Mr. Om Hirulkar <omhirulkar31@gmail.com>
2025-09-15 10:31:55 +0530

    Create SimpleCorsFilter.java

---
+++ src/main/java/com/codewithskills/IndusGram/SimpleCorsFilter.java
@@ -0,0 +1,22 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+public class SimpleCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        // Always set these headers
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        chain.doFilter(request, response);
+    }
+}
